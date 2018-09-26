2b48a6db872e45d6c3aa009233d76db78664ee2c
Christian Broberg <cb@webhouse.dk>
2018-09-26 19:09:13 +0200

    No comments

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var express = require("express")
 
 var app = express()
 
+app.get("/dispatch", (req, res, next) => {
+	// res.json(["Christian", "Mikkel", "Andrei", "Martin", "Henrik"])
+	res.json("Now dispatching service messages")
+	console.log("Now dispatching service messages")
+})
+
 app.listen(3000, () => {
 	console.log("Server running on port 3000")
 })
