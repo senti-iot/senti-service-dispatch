7fb90f69ccccf882d9dabf1a7a9adb8eeebea5d1
Christian Broberg <cb@webhouse.dk>
2018-09-27 13:58:10 +0200

    Comit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env nodejs
 var express = require("express")
 
+var port = process.env.PORT || 3000
+
 var app = express()
 
 app.get("/", (req, res, next) => {	
@@ -8,8 +10,20 @@ app.get("/", (req, res, next) => {
 	console.log("Now dispatching service messages")
 })
 
-var port = process.env.PORT || 80
 
 app.listen(port, () => {
 	console.log("Server running on port: ", port)
 })
+
+
+
+
+/* var http = require('http')
+
+http.createServer(function (req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' })
+	res.end('Hello World\n')
+}).listen(8080, 'localhost')
+console.log('Server running at http://localhost:8080/')
+
+ */
