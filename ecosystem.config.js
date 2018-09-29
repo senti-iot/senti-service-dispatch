999972f3ee0ca043303dc8988f4e6cc23641db72
Christian Broberg <cb@webhouse.dk>
2018-09-29 02:32:43 +0200

    ecosystem added

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	apps: [{
+		name: 'senti-service-dispatch',
+		script: 'index.js',
+
+		// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+		args: 'one two',
+		instances: max,
+		autorestart: true,
+		watch: true,
+		ignore_watch: "logs/*",
+		max_memory_restart: '1G',
+		env: {
+			NODE_ENV: 'development'
+		},
+		env_production: {
+			NODE_ENV: 'production'
+		}
+	}],
+}
+
