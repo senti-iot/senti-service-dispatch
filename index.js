bc0326819e4a770b926b49e3aa3dec23c32b38cd
Christian Broberg <cb@cb-pro.local>
2018-09-29 02:13:58 +0200

    new

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env nodejs
 var express = require("express")
+const bodyParser = require('body-parser')
 
 var port = process.env.PORT || 3000
 
 var app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
 
 app.get("/dispatch", (req, res, next) => {	
 	res.json("Now dispatching service messages")
@@ -12,12 +18,15 @@ app.get("/dispatch", (req, res, next) => {
 
 app.post("/dispatch", (req, res, next) => {	
 	// res.json("Now dispatching service messages")
-	res.send('OK')
+    res.send('OK')
+    const theData = req.body;
 	console.log("Now dispatching service messages")
+    console.log(theData)
 })
 
 app.listen(port, () => {
-	console.log("Server running on port: ", port)
+    console.log("Server running on port: ", port)
+    console.log(`Server running on port: ${port}. Started ${new Date().toString()}`)
 })
 
 
