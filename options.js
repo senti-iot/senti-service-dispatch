2cd4a283cff5b0bd28c09585538f8aa3bf294a02
Christian Broberg <cb@webhouse.dk>
2018-10-19 00:07:28 +0200

    Split options and MQTT options into seperate endpoints and objects

--- options.js
+++ options.js
@@ -18,19 +18,6 @@ const mqttOptions = {
 }
 
 const options = {
-	host: 'mqtt://hive.senti.cloud',
-	port: '1883',
-	username: '',
-	password: '',
-	keepalive: 60,
-	clientId: hostname,
-	clean: true,
-	will: {
-		topic: 'sensor/status',
-		payload: 'offline',
-		qos: 1,
-		retain: false
-	},
 	slackChannel: 'https://hooks.slack.com/services/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',
 	logLocale: 'da'
 }
